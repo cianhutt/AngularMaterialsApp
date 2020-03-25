3715744bdb0a5f7c1d6ef7dbf55eeda2c54514a3
Cian Hutt <cian.hutt@mycit.ie>
2020-03-25 16:27:47 +0000

    Add Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { PiitrackingComponent } from './piitracking/piitracking.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiitrackingComponent,
+    LoginComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
