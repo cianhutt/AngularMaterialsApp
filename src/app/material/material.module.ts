2fa495c14db571d45669b328c342063488a6d6fd
Cian Hutt <cian.hutt@mycit.ie>
2020-03-28 14:15:17 +0000

    Create angular material module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,42 @@
+import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  imports: [
+  CommonModule, 
+  MatToolbarModule,
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatTableModule,
+  MatDialogModule,
+  MatMenuModule,
+  MatIconModule,
+  MatProgressSpinnerModule
+  ],
+  exports: [
+  CommonModule,
+   MatToolbarModule, 
+   MatButtonModule, 
+   MatCardModule, 
+   MatInputModule, 
+   MatDialogModule,
+   MatMenuModule,
+   MatIconModule,
+   MatProgressSpinnerModule,
+   MatFormFieldModule,
+   MatInputModule,
+   MatTableModule
+   ],
+})
+export class CustomMaterialModule { }
