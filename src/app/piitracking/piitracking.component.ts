70210ae5847ebcda3e3c6fe0101ea25f508a6145
Cian Hutt <cian.hutt@mycit.ie>
2020-04-09 16:38:46 +0100

    Remove file table and add material table

--- src/app/piitracking/piitracking.component.ts
+++ src/app/piitracking/piitracking.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface File {
+  server: string;
+  fileName: string;
+  pii: string;
+  full: string;
+}
+
+const files: File[] = [
+  {server: 'FS-10', fileName: 'JohnSmithResume.doc', pii: 'Name', full:'C:/John/JohnSmithResume.doc'},
+  {server: 'FS-12', fileName: 'JohnSmithResume.pdf', pii: 'Name', full:'C:/John/JohnSmithResume.pdf'},
+  {server: 'FS-14', fileName: 'MaryLeeResume.pdf', pii: 'PIN', full:'C:/Mary/MaryLeeResume.pdf'},
+  {server: 'FS-21', fileName: 'HousingApplication.pdf', pii: 'Name', full:'C:/Dave/HousingApplication.pdf'},
+  {server: 'FS-42', fileName: 'RenewalForm.pdf', pii: 'Type', full:'C:/Lucy/RenewalForm.pdf'},
+  {server: 'FS-21', fileName: 'Tax-2019.pdf', pii: 'Name', full:'C:/Tom/Tax-2019.pdf'},
+  {server: 'FS-21', fileName: 'Tax-2018.pdf', pii: 'Name', full:'C:/Greg/Tax-2018.pdf'},
+];
+
+
 @Component({
   selector: 'app-piitracking',
   templateUrl: './piitracking.component.html',
@@ -7,20 +25,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiitrackingComponent implements OnInit {
 
+  displayedColumns: string[] = ['server', 'fileName', 'pii', 'full'];
+  dataSource = files;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  files = [
-    {server: 'FS-10', filename: 'JohnSmithResume.doc', pii: 'Name', full:'C:/John/JohnSmithResume.doc'},
-    {server: 'FS-12', filename: 'JohnSmithResume.pdf', pii: 'Name', full:'C:/John/JohnSmithResume.pdf'},
-    {server: 'FS-14', filename: 'MaryLeeResume.pdf', pii: 'PIN', full:'C:/Mary/MaryLeeResume.pdf'},
-    {server: 'FS-21', filename: 'HousingApplication.pdf', pii: 'Name', full:'C:/Dave/HousingApplication.pdf'},
-    {server: 'FS-42', filename: 'RenewalForm.pdf', pii: 'Type', full:'C:/Lucy/RenewalForm.pdf'},
-    {server: 'FS-21', filename: 'Tax-2019.pdf', pii: 'Name', full:'C:/Tom/Tax-2019.pdf'},
-    {server: 'FS-21', filename: 'Tax-2018.pdf', pii: 'Name', full:'C:/Greg/Tax-2018.pdf'},
-];
+//   files = [
+//     {server: 'FS-10', filename: 'JohnSmithResume.doc', pii: 'Name', full:'C:/John/JohnSmithResume.doc'},
+//     {server: 'FS-12', filename: 'JohnSmithResume.pdf', pii: 'Name', full:'C:/John/JohnSmithResume.pdf'},
+//     {server: 'FS-14', filename: 'MaryLeeResume.pdf', pii: 'PIN', full:'C:/Mary/MaryLeeResume.pdf'},
+//     {server: 'FS-21', filename: 'HousingApplication.pdf', pii: 'Name', full:'C:/Dave/HousingApplication.pdf'},
+//     {server: 'FS-42', filename: 'RenewalForm.pdf', pii: 'Type', full:'C:/Lucy/RenewalForm.pdf'},
+//     {server: 'FS-21', filename: 'Tax-2019.pdf', pii: 'Name', full:'C:/Tom/Tax-2019.pdf'},
+//     {server: 'FS-21', filename: 'Tax-2018.pdf', pii: 'Name', full:'C:/Greg/Tax-2018.pdf'},
+// ];
 
-  // file = new File("rerb","ferbeb","fetb","fetb");
+
 }
