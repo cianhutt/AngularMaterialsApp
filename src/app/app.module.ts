04a1eb28f96ed1d8eff24ca8128ad6a7e0670c25
Cian Hutt <cian.hutt@mycit.ie>
2020-05-09 14:05:06 +0100

    Add routerLink directive to navigate to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PiitrackingComponent } from './piitracking/piitracking.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { CustomMaterialModule } from './material/material.module';
@@ -15,8 +12,7 @@ import {ChartsModule} from 'ng2-charts';
 @NgModule({
   declarations: [
     AppComponent,
-    PiitrackingComponent,
-    LoginComponent,
+    routingComponents,
     HeaderComponent,
     HomeComponent
   ],
